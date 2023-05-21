7ddd5e185b6f9e3a8675aceae62753cce78daef9
darkcaesar0721 <darkcaesar0721@gmail.com>
2023-05-22 04:28:01 +0900

    - Slider style class change

--- src/components/AppSlider.vue
+++ src/components/AppSlider.vue
@@ -21,9 +21,9 @@
               max-width="800"
               style="left: -200px"
           >
-            <v-card-title class="text-uppercase">Join the community</v-card-title>
-            <v-card-text class="pt-7">We're Building a Better</v-card-text>
-            <v-card-text class="pt-12">World Together</v-card-text>
+            <v-card-title class="text-uppercase slider-body-title">Join the community</v-card-title>
+            <v-card-text class="pt-7 slider-body-text">We're Building a Better</v-card-text>
+            <v-card-text class="pt-12 slider-body-text">World Together</v-card-text>
             <v-btn class="mt-lg-12 text--white text-uppercase ms-4 slider-body-btn" color="green" width="250" height="54">Make An Appointment</v-btn>
           </v-card>
         </v-container>
@@ -44,9 +44,9 @@
               max-width="800"
               style="left: -200px"
           >
-            <v-card-title class="text-uppercase">Tramquility and peace</v-card-title>
-            <v-card-text class="pt-7">Harmony Inside Brings</v-card-text>
-            <v-card-text class="pt-12">Joy to the World</v-card-text>
+            <v-card-title class="text-uppercase slider-body-title">Tramquility and peace</v-card-title>
+            <v-card-text class="pt-7 slider-body-text">Harmony Inside Brings</v-card-text>
+            <v-card-text class="pt-12 slider-body-text">Joy to the World</v-card-text>
             <v-btn class="mt-lg-12 text--white text-uppercase ms-4 slider-body-btn" color="green" width="250" height="54">Make An Appointment</v-btn>
           </v-card>
         </v-container>
@@ -67,9 +67,9 @@
               max-width="800"
               style="left: -200px"
           >
-            <v-card-title class="text-uppercase">Learning Bible</v-card-title>
-            <v-card-text class="pt-7">We Aim to Be Guided</v-card-text>
-            <v-card-text class="pt-12">by God's Word</v-card-text>
+            <v-card-title class="text-uppercase slider-body-title">Learning Bible</v-card-title>
+            <v-card-text class="pt-7 slider-body-text">We Aim to Be Guided</v-card-text>
+            <v-card-text class="pt-12 slider-body-text">by God's Word</v-card-text>
             <v-btn class="mt-lg-12 text--white text-uppercase ms-4 slider-body-btn" color="green" width="250" height="54">Make An Appointment</v-btn>
           </v-card>
         </v-container>
@@ -100,7 +100,7 @@ export default {
   color: white !important;
 }
 
-.v-card__title {
+.slider-body-title {
   color: white !important;
   font-family: "Crimson Pro",serif !important;
   font-size: 16px !important;
@@ -108,7 +108,7 @@ export default {
   letter-spacing: 2px !important;
 }
 
-.v-card__text {
+.slider-body-text {
   color: white !important;
   font-family: "Crimson Pro",serif !important;
   font-size: 80px !important;
